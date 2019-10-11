b5953b3c1b6d1f0d088ec1ab6d9309e06ef41bdb
BB <b.boilevin@wanadoo.fr>
2019-10-11 21:16:44 +0200

    RL1000 prog

---
+++ RL1000/ardumower/ardumower.ino
@@ -0,0 +1,73 @@
+/*      
+ *       
+ *       
+ *       
+ *       DEV VERSION FOR ROBOMOW RL1000 with 3 BTS7960 motor driver and 3 ina226 with R010 shunt 
+ *       
+ *       
+ *       
+ *       
+ *       
+ *      DUE and ODOMETRY MANDATORY VERSION 
+ *      PCB1.3 
+ *      FOR BY LANE USE, IMU GY-88 MANDATORY 
+ *      
+ *      Use DUE programming port for programming and PC console!
+ *      
+ *      --------------------------------- GPS ----------------------------------------------------
+ *      GPS Change speed according to your module and connected to P44
+ *      #define GPS_BAUDRATE  38400  // 9600 for M6n ,19200 or 38400 for BN880,M8Nother module
+ *      
+ *      ------------------------------ RASPBERRY -------------------------------------------------
+ *      If Raspberry PI is not connected change into mower.h
+ *          #define Console Serial
+ *          //#define Console SerialUSB
+ *      and into arduremote setting R/C set raspberryPiuse to NO
+ *      then you have access to the Console on the PC 
+ *      
+ *      If Raspberry PI is connected change into mower.h
+ *          //#define Console Serial
+ *          #define Console SerialUSB
+ *      and into arduremote setting R/C set raspberryPiuse to YES
+ *      ------------------------------------------------------------------------------------------ 
+  Ardumower (www.ardumower.de)
+  Copyright (c) 2013-2015 by Alexander Grau
+  Copyright (c) 2013-2015 by Sven Gennat
+  Copyright (c) 2014 by Maxime Carpentieri    
+  Copyright (c) 2014-2015 by Stefan Manteuffel
+  Copyright (c) 2015 by Uwe Zimprich
+  
+  Private-use only! (you need to ask for a commercial-use)
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  
+  Private-use only! (you need to ask for a commercial-use)
+
+
+  Documentation:  http://wiki.ardumower.de
+    
+ */
+#include <Arduino.h>
+#include <Wire.h>
+#include "config.h"
+
+
+
+void setup()  {     
+  robot.setup();
+} 
+
+void loop()  {     
+  robot.loop();    
+}
