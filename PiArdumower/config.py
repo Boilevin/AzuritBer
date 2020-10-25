b5d47c655c4cdb85bee8caa1e49b22c83390d62c
GitBern <b.boilevin@wanadoo.fr>
2020-10-25 20:21:19 +0100

    mqtt freeze

--- PiArdumower/config.py
+++ PiArdumower/config.py
@@ -20,9 +20,11 @@ if (os.name=='posix'):
     #the battery cycle can be recorded on each charging cycle see plot folder
     AutoRecordBatCharging=True
     #Possible to remove the in and out NMEA message visualisation in console
-    useDebugConsole=True
+    useDebugConsole=False
     
     #On multiple area it's the sender IP, Use to start and stop them over HTTP
+    #Sender1AdressIP is the station sender 
+    
     Sender1AdressIP="10.0.0.150"
     Sender2AdressIP="10.0.0.151"
     Sender3AdressIP="10.0.0.154"
@@ -32,13 +34,16 @@ if (os.name=='posix'):
     streamVideoOnPower = True #auto start of the streaming
 
     #Setting for Mqtt and home automation
-    useMqtt=False
+    useMqtt=True
     Mqtt_Broker_IP="10.0.0.8"
     Mqtt_Port = 1883
+    Mqtt_User = "admin"
+    Mqtt_Password = "admin"
+    Mqtt_ShowDebug = False
     #delay in second between 2 sends of the DUE loops/sec over mqtt
-    Mqtt_IdleFreqency = 5
-    #if more than one mower use different name
-    Mqtt_MowerName = "Denna"
+    Mqtt_IdleFreqency = 1
+    # Always first letter uppercase and set the yaml file according Mower is the defaut name
+    Mqtt_MowerName = "Mower"
 
 
 
